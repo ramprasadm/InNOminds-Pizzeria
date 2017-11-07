7585ce43d7992221a30e78b4b0f2eded74b053e6
ramprasadm <ramprasad.marrapu@gmail.com>
2017-11-08 01:11:35 +0530

    Pizza and topping and review order

--- js/script.js
+++ js/script.js
@@ -89,3 +89,93 @@ function saveBillingInformation() {
         response.render('regSuccess');
    });
 }
+
+var selectedPizzas = [];
+var selectedToppings = [];
+
+function addRemovePizza(pizzaDetails){
+    var checked = document.getElementById(pizzaDetails.checkboxid).checked;
+    console.log("checkbox checked: "+checked);
+    if(checked){
+        selectedPizzas.push(pizzaDetails);
+        document.getElementById(pizzaDetails.quantityid).removeAttribute("disabled");
+        document.getElementById(pizzaDetails.quantityid).value = "1";
+       // document.getElementById(pizzaDetails.quantityid).option[0].disabled = "disabled";
+    }
+    else{
+        var deleteIndex = -1;
+        for(i=0; i<selectedPizzas.length; i++){
+            if(selectedPizzas[i].checkboxid == pizzaDetails.checkboxid){
+                deleteIndex = i;
+                document.getElementById(pizzaDetails.quantityid).disabled = "disabled";
+                document.getElementById(pizzaDetails.quantityid).value = "";
+            }
+        }
+        if(deleteIndex != -1){
+            selectedPizzas.splice(deleteIndex, 1);
+        }
+    }
+    console.log("selected Pizzas : "+selectedPizzas);
+}
+
+function updateQuantity(pizzaQuantity){
+        for(i=0; i<selectedPizzas.length; i++){
+            if(selectedPizzas[i].checkboxid == pizzaQuantity.checkboxid){
+                selectedPizzas[i].quantity = document.getElementById(pizzaQuantity.quantityid).value;
+            }
+        }
+}
+
+function loadSelectedPizzas(){
+    for(i=0; i<selectedPizzas.length; i++){
+            document.getElementById(selectedPizzas[i].quantityid).removeAttribute("disabled");
+            document.getElementById(selectedPizzas[i].quantityid).value = selectedPizzas[i].quantity;
+            document.getElementById(selectedPizzas[i].checkboxid).checked = "checked";
+        }
+}
+
+function addRemoveTopping(toppingDetails){
+    var checked = document.getElementById(toppingDetails.checkboxid).checked;
+    console.log("checkbox checked: "+checked);
+    if(checked){
+        selectedToppings.push(toppingDetails);
+    }
+    else{
+        var deleteIndex = -1;
+        for(i=0; i<selectedPizzas.length; i++){
+            if(selectedToppings[i].checkboxid == toppingDetails.checkboxid){
+                deleteIndex = i;
+            }
+        }
+        if(deleteIndex != -1){
+            selectedToppings.splice(deleteIndex, 1);
+        }
+    }
+    console.log("selected Toppings : "+selectedToppings);
+}
+function loadSelectedToppings(){
+    for(i=0; i<selectedToppings.length; i++){
+            document.getElementById(selectedToppings[i].checkboxid).checked = "checked";
+        }
+}
+
+function loadPendingOrders(){
+    var tr = "";
+    var total = 0;
+    for(i=0; i<selectedPizzas.length; i++){
+         tr += "<tr><td>"+selectedPizzas[i].name+"</td>";
+         tr += "<td>"+selectedPizzas[i].quantity+"</td>";
+         tr += "<td style='align:right;'>"+selectedPizzas[i].price+"</td>";
+         tr += "<td style='align:right;'>"+(selectedPizzas[i].quantity*selectedPizzas[i].price)+"</td></tr>";
+         total += (selectedPizzas[i].quantity*selectedPizzas[i].price);
+    }
+    for(i=0; i<selectedToppings.length; i++){
+         tr += "<tr><td>"+selectedToppings[i].name+"</td>";
+         tr += "<td></td>";
+         tr += "<td style='align:right;'>"+selectedToppings[i].price+"</td>";
+         tr += "<td style='align:right;'>"+selectedToppings[i].price+"</td></tr>";
+         total += selectedToppings[i].price;
+    }
+    tr += "<tr><td>Total</td><td></td><td></td><td style='align:right;'>"+total+"</td></tr>"
+    document.getElementById("pendingorders").innerHTML = tr;
+}
