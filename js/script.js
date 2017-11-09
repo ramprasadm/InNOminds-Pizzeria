aafc54f5cbea5c24c7cfba6358926dcab9be4cf2
SHAIK RIYAZ BASHA <33252083+RIYAZBASHASHAIK@users.noreply.github.com>
2017-11-09 17:11:06 +0530

    Billing code changes

--- js/script.js
+++ js/script.js
@@ -44,50 +44,13 @@ function setCheckedValue(groupName, value) {
     return null;
 }
 
-function loadBillingInformation() {
-    // this will be service call to fetch customer data based on customer Id
-    var customer = {
-        "firstName": "Derek",
-        "lastName": "Puckett",
-        "phone": "(954) 594-9355",
-        "email": "derek.puckett@vulputate.net",
-        "address": {
-            "street": "P.O. Box 914, 9990 Dapibus St.",
-            "city": "Quam",
-            "state": "OH",
-            "zip": "55154",
-            "country": "USA"
-        },
-        "_id": {
-            "$oid": "51f06ded06a7baa417000001"
-        }
-    };
-    // country and address1 and address2 need to be added to Billing information table
-    setFormElementValue("address1", customer.address.street);
-    setFormElementValue("address2", customer.address.street);
-    setFormElementValue("city", customer.address.city);
-    setFormElementValue("state", customer.address.state);
-    setFormElementValue("zip", customer.address.zip);
-    setFormElementValue("country", customer.address.country);
-}
-
-function saveBillingInformation() {
-    db.insert({
-        street: document.getElementById("address1").value,
-        state: document.getElementById("state").value,
-        country: document.getElementById("country").value,
-        zip: document.getElementById("zip").value,
-        city: document.getElementById("city").value
-    }, function (err, doc) {
-        if (err) {
-            console.log(err);
-            return response.sendStatus(500);
-
-        } else {
-          console.log("Billing Information Saved", doc);
-        }
-        response.render('regSuccess');
-   });
+function loadBillingInformation(data) {
+    setFormElementValue("address1", data.address1);
+    setFormElementValue("address2", data.address2);
+    setFormElementValue("city", data.city);
+    setFormElementValue("state", data.state);
+    setFormElementValue("zip", data.zip);
+    setFormElementValue("country", data.country);
 }
 
 var selectedPizzas = [];
