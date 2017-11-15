c40c4e29825d2481a00a7a0a56a150b8de3c0df1
bp117 <bindupriya117@gmail.com>
2017-11-15 11:32:25 +0530

    Add files via upload

--- js/lib/stt-token.js
+++ js/lib/stt-token.js
@@ -7,8 +7,8 @@ var express = require('express'),
 var sttConfig = extend({
   version: 'v1',
   url: 'https://stream.watsonplatform.net/speech-to-text/api',
-  username: process.env.STT_USERNAME || '32e59008-80fc-45e1-b96a-70b8555bc989',
-  password: process.env.STT_PASSWORD || 'jpOaOgH8auBd',
+  username: process.env.STT_USERNAME || 'c6176efe-8ba9-46f2-9d6b-e8aa35e6e6a4',
+  password: process.env.STT_PASSWORD || 'BVzycSly2H0Y',
 }, vcapServices.getCredentials('speech_to_text'));
 
 var sttAuthService = watson.authorization(sttConfig);
